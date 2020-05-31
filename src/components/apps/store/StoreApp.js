2fa42e2263317daa2eaffb2f3325ad4992f183fb
misshu1 <nedelusandrei@gmail.com>
2020-05-31 21:03:15 +0300

    added error message when getting store products

--- src/components/apps/store/StoreApp.js
+++ src/components/apps/store/StoreApp.js
@@ -1,125 +1,41 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
 import { useFirebaseContext } from '../../../contexts/firebaseContext';
+import { useNotificationsContext } from '../../../contexts/notificationsContext';
 import FolderApp from '../../folder/FolderApp';
 import ProductApp from './product/ProductApp';
 import { Container } from './style';
 
-// const store = [
-//     {
-//         id: uuid.v4(),
-//         title: 'some title',
-//         image: 'https://via.placeholder.com/240',
-//         price: 49.99,
-//         discount: 24.99,
-//         averageRating: 4.5,
-//         description:
-//             'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dicta laudantium impedit architecto blanditiis pariatur vitae quae sed nostrum similique?',
-//         reviews: [
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Arnold Shisad',
-//                 rating: 4,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content: 'something here',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Muhsfgo Shsigtyad',
-//                 rating: 5,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content:
-//                     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, deserunt.',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Arnold Shisad',
-//                 rating: 4,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content: 'something here',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Muhsfgo Shsigtyad',
-//                 rating: 5,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content:
-//                     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, deserunt.',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Arnold Shisad',
-//                 rating: 4,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content: 'something here',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Muhsfgo Shsigtyad',
-//                 rating: 5,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content:
-//                     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, deserunt.',
-//             },
-//         ],
-//     },
-//     {
-//         id: uuid.v4(),
-//         title: 'some title',
-//         image: 'https://via.placeholder.com/240',
-//         price: 49.99,
-//         discount: 24.99,
-//         averageRating: 4.5,
-//         description:
-//             'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit dicta laudantium impedit architecto blanditiis pariatur vitae quae sed nostrum similique?',
-//         reviews: [
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Arnold Shisad',
-//                 rating: 4,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content: 'something here',
-//             },
-//             {
-//                 id: uuid.v4(),
-//                 userDisplayName: 'Muhsfgo Shsigtyad',
-//                 rating: 5,
-//                 date: firebase.getFirebaseTimestamp(),
-//                 content:
-//                     'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, deserunt.',
-//             },
-//         ],
-//     },
-// ];
-
 const StoreApp = () => {
     const { firestore } = useFirebaseContext();
     const [products, setProducts] = useState([]);
-
-    // const getProducts = useCallback(async () => {
-    //     const test = await firestore
-    //         .collection('store')
-    //         .get()
-    //         .then((data) => console.log(data.data()));
-    // }, [firestore]);
+    const { showError } = useNotificationsContext();
 
     const getProducts = useCallback(async () => {
         let dbProducts = [];
         try {
             const productsRef = await firestore.collection('products').get();
-            await productsRef.forEach(
-                (doc) =>
-                    (dbProducts = [
-                        ...dbProducts,
-                        { id: doc.id, ...doc.data() },
-                    ])
-            );
+            if (productsRef.size) {
+                await productsRef.forEach(
+                    (doc) =>
+                        (dbProducts = [
+                            ...dbProducts,
+                            { id: doc.id, ...doc.data() },
+                        ])
+                );
+                setProducts(dbProducts);
+            } else {
+                throw new Error();
+            }
         } catch (err) {
-            // TODO add custom notification alert
-            console.log(err);
+            showError(
+                'Error',
+                'Failed to get store products from database!',
+                500
+            );
         }
-        setProducts(dbProducts);
-    }, [firestore]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     useEffect(() => {
         getProducts();
