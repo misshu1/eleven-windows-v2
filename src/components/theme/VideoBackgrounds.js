d1e1e641e75aa091c1c9199d3b492c8711952bd2
misshu1 <nedelusandrei@gmail.com>
2020-06-12 20:06:02 +0300

    added video previews

--- src/components/theme/VideoBackgrounds.js
+++ src/components/theme/VideoBackgrounds.js
@@ -1,3 +1,6 @@
+import fantasy_world_preview from '../../assets/images/videoPreview/fantasy_world.jpg';
+import illidan_stormrage_preview from '../../assets/images/videoPreview/illidan_stormrage.jpg';
+import wistful_by_omario_preview from '../../assets/images/videoPreview/wistful_by_omario.jpg';
 import fantasy_world_mp4 from '../../assets/videos/fantasy_world.mp4';
 import fantasy_world_webm from '../../assets/videos/fantasy_world.webm';
 import illidan_stormrage_mp4 from '../../assets/videos/illidan_stormrage.mp4';
@@ -14,6 +17,7 @@ export const videoBackgrounds = [
             webm: illidan_stormrage_webm,
             mp4: illidan_stormrage_mp4,
         },
+        preview: illidan_stormrage_preview,
     },
     {
         id: 'vid2',
@@ -23,6 +27,7 @@ export const videoBackgrounds = [
             webm: fantasy_world_webm,
             mp4: fantasy_world_mp4,
         },
+        preview: fantasy_world_preview,
     },
     {
         id: 'vid3',
@@ -32,5 +37,6 @@ export const videoBackgrounds = [
             webm: wistful_by_omario_webm,
             mp4: wistful_by_omario_mp4,
         },
+        preview: wistful_by_omario_preview,
     },
 ];
