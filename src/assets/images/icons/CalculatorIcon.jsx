6e51471df0ddcd606a1afb9429a6ef7eb2641b90
misshu1 <nedelusandrei@gmail.com>
2020-12-02 13:23:13 +0200

    updated icons

--- src/assets/images/icons/CalculatorIcon.jsx
+++ src/assets/images/icons/CalculatorIcon.jsx
@@ -9,39 +9,34 @@ export function CalculatorIcon({ width, height }) {
         <svg
             key={getSelectedTheme().id}
             xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 750 750'
             style={{ width, height }}
+            fill='none'
+            viewBox='0 0 750 750'
         >
-            <g fill='#fff'>
-                <path d='M0 0h375v375H0z' />
-                <path d='M375 0h375v375H375z' />
-                <path d='M0 375h375v375H0z' />
-            </g>
+            <path fill='#fff' d='M100 100H650V650H100z'></path>
+            <path fill='var(--primary)' d='M375 375H650V650H375z'></path>
             <path
-                d='M375 375h375v375H375z'
-                fill='var(--primary)'
-                style={{ transition: 'fill 0.2s ease-in-out' }}
-            />
-            <g fill='none'>
-                <g stroke='#000'>
-                    <g strokeWidth='25' strokeMiterlimit='10'>
-                        <path d='M90.6 659.4l193.1-193.1' />
-                        <path d='M90.6 465.6l193.8 193.8M187.5 72.8v229.7' />
-                        <path d='M73.5 187.5h230' />
-                    </g>
-                    <path
-                        d='M484.8 187.7h156.4'
-                        strokeMiterlimit='10'
-                        strokeWidth='30'
-                    />
-                </g>
-                <path
-                    d='M484.8 519.5h156.4m-156.4 84h156.4'
-                    strokeWidth='30'
-                    strokeMiterlimit='10'
-                    stroke='#fff'
-                />
-            </g>
+                fill='#fff'
+                fillRule='evenodd'
+                d='M606 458H419v30h187v-30zm0 80H419v30h187v-30z'
+                clipRule='evenodd'
+            ></path>
+            <path fill='#fff' d='M100 375H375V650H100z'></path>
+            <path
+                fill='#000'
+                fillRule='evenodd'
+                d='M181.721 435.279l-21.213 21.213L216.016 512l-55.508 55.508 21.213 21.213 55.508-55.508 55.508 55.508 21.213-21.213L258.442 512l55.508-55.508-21.213-21.213-55.508 55.508-55.508-55.508z'
+                clipRule='evenodd'
+            ></path>
+            <path fill='#fff' d='M375 100H650V375H375z'></path>
+            <path fill='#000' d='M419 223H606V253H419z'></path>
+            <path fill='#fff' d='M100 100H375V375H100z'></path>
+            <path
+                fill='#000'
+                fillRule='evenodd'
+                d='M252 144h-30v79h-78v30h78v78h30v-78h79v-30h-79v-79z'
+                clipRule='evenodd'
+            ></path>
         </svg>
     );
 }
